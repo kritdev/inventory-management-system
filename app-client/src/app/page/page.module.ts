3a2e88cc7dfe1539b255644b22eae9212c904b8a
Krit <kritdevelop@gmail.com>
2021-06-21 20:59:35 +0700

    create main component

--- app-client/src/app/page/page.module.ts
+++ app-client/src/app/page/page.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home/home.component';
+import { MainComponent } from './layout/main/main.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    MainComponent
   ],
   imports: [
     SharedModule
