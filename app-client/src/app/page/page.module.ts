b3ff7a3cfb10907fcac95d5d51aee128cd1024fc
Krit <kritdevelop@gmail.com>
2021-06-21 21:14:46 +0700

    create navbar

--- app-client/src/app/page/page.module.ts
+++ app-client/src/app/page/page.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { MainComponent } from './layout/main/main.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    MainComponent
+    MainComponent,
+    NavbarComponent
   ],
   imports: [
     SharedModule
