49760aa2b66e1cd258d71216c519c2372b859598
Krit <kritdevelop@gmail.com>
2021-07-14 17:08:00 +0700

    inventory-log component - create component

---
+++ app-client/src/app/component/inventory-log/inventory-log.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-log',
+  templateUrl: './inventory-log.component.html',
+  styleUrls: ['./inventory-log.component.css']
+})
+export class InventoryLogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
