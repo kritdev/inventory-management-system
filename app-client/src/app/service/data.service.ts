f1180d386e7dd7c3406ee22e63bae899375d4004
Krit <kritdevelop@gmail.com>
2021-07-14 16:55:19 +0700

    data service - refactor data

--- app-client/src/app/service/data.service.ts
+++ app-client/src/app/service/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IProductSummary } from '../entity/product-summary.model';
-import { imageDataList } from './sample-data';
+import { sampleDataList } from './sample-data';
 
 @Injectable({
   providedIn: 'root'
@@ -9,127 +9,11 @@ export class DataService {
 
   constructor() { }
 
+  public retrieveProductSummary(id: number): IProductSummary {
+    return sampleDataList[id - 101];
+  }
+
   public retrieveProductSummaryList(): IProductSummary[] {
-    return [
-      {
-        id: 101,
-        name: 'Small Chair',
-        brand: 'Chicky Chair',
-        category: {id:301, name:'Chair'},
-        unitOfMeasure: {id:401, name:'pcs.'},
-        stockItems: [{
-          id: 501,
-          countInStock: 10,  
-        }],
-        images: [{
-          id: 601,
-          imageData: imageDataList[1],
-          imageDataContentType: "image/png",
-          defaultImage: true,
-        }],
-      },
-      {
-        id: 102,
-        name: 'Dummy Chair',
-        brand: 'Chicky Chair',
-        category: {id:301, name:'Chair'},
-        unitOfMeasure: {id:401, name:'pcs.'},
-        stockItems: [{
-          id: 502,
-          countInStock: 3,  
-        }],
-        images: [{
-          id: 602,
-          imageData: imageDataList[0],
-          imageDataContentType: "image/png",
-          defaultImage: true,
-        }],
-      },
-      {
-        id: 101,
-        name: 'Small Chair',
-        brand: 'Chicky Chair',
-        category: {id:301, name:'Chair'},
-        unitOfMeasure: {id:401, name:'pcs.'},
-        stockItems: [{
-          id: 501,
-          countInStock: 10,  
-        }],
-        images: [{
-          id: 601,
-          imageData: imageDataList[1],
-          imageDataContentType: "image/png",
-          defaultImage: true,
-        }],
-      },
-      {
-        id: 101,
-        name: 'Small Chair',
-        brand: 'Chicky Chair',
-        category: {id:301, name:'Chair'},
-        unitOfMeasure: {id:401, name:'pcs.'},
-        stockItems: [{
-          id: 501,
-          countInStock: 10,  
-        }],
-        images: [{
-          id: 601,
-          imageData: imageDataList[1],
-          imageDataContentType: "image/png",
-          defaultImage: true,
-        }],
-      },
-      {
-        id: 101,
-        name: 'Small Chair',
-        brand: 'Chicky Chair',
-        category: {id:301, name:'Chair'},
-        unitOfMeasure: {id:401, name:'pcs.'},
-        stockItems: [{
-          id: 501,
-          countInStock: 10,  
-        }],
-        images: [{
-          id: 601,
-          imageData: imageDataList[1],
-          imageDataContentType: "image/png",
-          defaultImage: true,
-        }],
-      },
-      {
-        id: 101,
-        name: 'Small Chair',
-        brand: 'Chicky Chair',
-        category: {id:301, name:'Chair'},
-        unitOfMeasure: {id:401, name:'pcs.'},
-        stockItems: [{
-          id: 501,
-          countInStock: 10,  
-        }],
-        images: [{
-          id: 601,
-          imageData: imageDataList[1],
-          imageDataContentType: "image/png",
-          defaultImage: true,
-        }],
-      },
-      {
-        id: 101,
-        name: 'Small Chair',
-        brand: 'Chicky Chair',
-        category: {id:301, name:'Chair'},
-        unitOfMeasure: {id:401, name:'pcs.'},
-        stockItems: [{
-          id: 501,
-          countInStock: 10,  
-        }],
-        images: [{
-          id: 601,
-          imageData: imageDataList[1],
-          imageDataContentType: "image/png",
-          defaultImage: true,
-        }],
-      },
-    ];
+    return sampleDataList;
   }
 }
