9353f6789ae3caadb9e48a08e071369191670471
Krit <kritdevelop@gmail.com>
2021-07-21 10:21:05 +0700

    search-bar - call retrieveCategorieList()

--- app-client/src/app/component/search-bar/search-bar.component.ts
+++ app-client/src/app/component/search-bar/search-bar.component.ts
@@ -1,5 +1,9 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { finalize, map } from 'rxjs/operators';
+import { ICategory } from 'src/app/entity/category.model';
 import { IProductFilter, ProductFilter } from 'src/app/entity/product-filter.model';
+import { DataService } from 'src/app/service/data.service';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,14 +12,29 @@ import { IProductFilter, ProductFilter } from 'src/app/entity/product-filter.mod
 })
 export class SearchBarComponent implements OnInit {
 
+  isLoading = false;
+  categories: ICategory[] = [];
   productFilter: IProductFilter = new ProductFilter();
 
   @Output()
   OnFilter: EventEmitter<Object> = new EventEmitter();
 
-  constructor() { }
+  constructor(private dataService: DataService,) { }
 
   ngOnInit(): void {
+    this.retrieveCategories();
+  }
+
+  protected retrieveCategories() {
+    this.isLoading = true;
+    this.dataService.retrieveCategorieList()
+      .pipe(
+        finalize(() => this.isLoading = false)
+      )
+      .subscribe(
+        result => { this.categories = result as any; },
+        err => { alert(err); }
+      );
   }
 
   onProductChange(event) {
