afbf29e53d72c0829d38a77979c2a3e76cb962f5
Krit <kritdevelop@gmail.com>
2021-09-21 20:49:59 +0700

    client - create app-form module and components

---
+++ app-client/src/app/component/app-form/form-control-text/form-control-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-control-text',
+  templateUrl: './form-control-text.component.html',
+  styleUrls: ['./form-control-text.component.css']
+})
+export class FormControlTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ app-client/src/app/component/app-form/form-template-setting/form-template-setting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-template-setting',
+  templateUrl: './form-template-setting.component.html',
+  styleUrls: ['./form-template-setting.component.css']
+})
+export class FormTemplateSettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
