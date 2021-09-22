b70f6a8222ca96328f957f69623fc0220e0ff4a8
Krit <kritdevelop@gmail.com>
2021-09-22 17:08:31 +0700

    client - Reusable template-driven form for Category and UoM page

--- app-client/src/app/component/app-form/form-template-setting/form-template-setting.component.ts
+++ app-client/src/app/component/app-form/form-template-setting/form-template-setting.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ISetting } from 'src/app/entity/setting.model';
 
 @Component({
   selector: 'app-form-template-setting',
   templateUrl: './form-template-setting.component.html',
   styleUrls: ['./form-template-setting.component.css']
 })
-export class FormTemplateSettingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class FormTemplateSettingComponent {
+  @Input() vm: ISetting;
+  @Output() save = new EventEmitter();
+  
+  previousState(): void {
+    window.history.back();
   }
 
+  onSubmit() {
+    this.save.emit(this.vm);
+  }
 }
